67037705498199db305b7cd14fa9022ee58d778a
Dmitriy <rench.03@mail.ru>
2023-12-02 19:02:55 +0300

    Initial Commit

---
+++ CodesSubmissionApplication/CodesSubmissionApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodesSubmissionApplicationApp.swift
+//  CodesSubmissionApplication
+//
+//  Created by Дмитрий Цуприков on 02.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CodesSubmissionApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
